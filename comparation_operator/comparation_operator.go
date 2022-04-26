04c8aacaf69ec1b6e30565176d98e49fdeb49068
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-04-26 18:28:43 +0700

    Operasi perbandingan

---
+++ comparation_operator/comparation_operator.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name1 = "Yudi"
+	var name2 = "Budi"
+
+	var result = name1 == name2
+	fmt.Println(result)
+
+	var angka1 = 100
+	var angka2 = 200
+	fmt.Println(angka1 > angka2)
+	fmt.Println(angka1 < angka2)
+	fmt.Println(angka1 == angka2)
+	fmt.Println(angka1 != angka2)
+}
