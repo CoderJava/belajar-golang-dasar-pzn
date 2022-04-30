597e067309dda6a61c524f904017cba651f9299b
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-04-30 09:33:58 +0700

    Tambahkan keterangan mengenai studi kasus for loops

--- for/for.go
+++ for/for.go
@@ -4,26 +4,31 @@ import "fmt"
 
 func main() {
 
+	// Loop cara 1
 	// counter := 1
 	// for counter <= 10 {
 	// 	fmt.Println("Perulangan ke ", counter)
 	// 	counter++
 	// }
 
+	// Loop cara 2
 	// for counter := 1; counter <= 10; counter++ {
 	// 	fmt.Println("Perulangan ke", counter)
 	// }
 
 	// names := []string{"Eko", "Kurniawan", "Khannedy"}
 
+	// Iteration collection cara 1
 	// for i := 0; i < len(names); i++ {
 	// 	fmt.Println(names[i])
 	// }
 
+	// Iteration collection cara 2
 	// for index, name := range names {
 	// 	fmt.Println("index:", index, " & name:", name)
 	// }
 
+	// Iteration collection cara 3
 	// for _, name := range names {
 	// 	fmt.Println("name:", name)
 	// }
@@ -32,6 +37,7 @@ func main() {
 	person["name"] = "Eko"
 	person["title"] = "Programmer"
 
+	// Iteration map
 	for key, value := range person {
 		fmt.Println(key, "=", value)
 	}
