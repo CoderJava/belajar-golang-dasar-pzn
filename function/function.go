c588370ee92029a3747cd0e9aa0a6711d4ef23e4
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-04-30 09:54:42 +0700

    Function parameter

--- function/function.go
+++ function/function.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	sayHello()
+	sayHelloTo("Yudi", "Setiawan")
 }
 
 func sayHello() {
 	fmt.Println("Hello World")
+}
+
+func sayHelloTo(firstName string, lastName string) {
+	fmt.Println("Hello", firstName, lastName)
 }
