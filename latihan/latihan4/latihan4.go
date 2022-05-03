4d0dbf0b2c14dc03cc5a954eb7ee0d6bd85adc3c
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-05-03 10:28:56 +0700

    Latihan 4

---
+++ latihan/latihan4/latihan4.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 21
+	y := 17
+	fmt.Println("Nilai x =", x)
+	fmt.Println("Nilai y =", y)
+	fmt.Println("=============")
+
+	x -= 5
+	y += 3
+	fmt.Println("Nilai x =", x)
+	fmt.Println("Nilai y =", y)
+}
