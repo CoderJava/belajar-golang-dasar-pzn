64c2ba064085e920ebff865fa842e2402faac43d
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-05-05 15:52:03 +0700

    Komentar

---
+++ komentar/komentar.go
@@ -0,0 +1,18 @@
+package main
+
+/**
+Komentar
+multi line
+tidak terbatas
+*/
+func main() {
+	// Ini komentar single line
+}
+
+/*
+Komentar multi line
+kayak gini juga bisa
+*/
+func tes() {
+
+}
