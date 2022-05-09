097da2a25af50c03ae529fc9193c4cd831973378
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-05-09 13:48:44 +0700

    Access modifier

--- helper/say_hello.go
+++ helper/say_hello.go
@@ -2,6 +2,18 @@ package helper
 
 import "fmt"
 
+// exported berarti bisa diakses dari luar package
+// unexported berarti tidak bisa diakses dari luar package
+
+// var versi = 1 // private (unexported)
+var Application = "Belajar Golang" // public (exported)
+
+// public (exported)
 func SayHello(name string) {
 	fmt.Println("Hello", name)
 }
+
+// private (unexported)
+// func sayGoodbye(name string) {
+// 	fmt.Println("Goodbye", name)
+// }
--- import/import.go
+++ import/import.go
@@ -1,10 +1,16 @@
 package main
 
-import "belajar-golang-dasar/helper"
-
+import (
+	"belajar-golang-dasar/helper"
+	"fmt"
+)
 
 func main() {
 	helper.SayHello("Yudi Setiawan")
+	// helper.sayGoodbye("Yudi Setiawan") // error
+
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.versi) // error
 }
 
 // Ini GO111MODULE-nya dalam kondisi off agar bisa berjalan.
